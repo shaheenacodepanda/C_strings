11a88273bfc8dbdfabb7cc978d8410fa74afcdcb
shaheenacodepanda <95137817+shaheenacodepanda@users.noreply.github.com>
2024-09-01 20:24:31 +0530

    Update word_rev.c

--- word_rev.c
+++ word_rev.c
@@ -36,3 +36,39 @@ int length = strlen(name);
     return 0;
     
 }
+
+/*
+
+#include <stdio.h>
+#include <string.h>
+//#define strlenn( char &name) char*(&name) - char*(&name+1)
+void str_rev( char *name, int initial, int final)
+{   char temp;
+    for( int i = initial ; i < final ; i++, final--)
+    {   temp = *(name+final);
+        *(name+final) = *(name+i);
+        *(name+i) = temp;
+    }
+}
+
+int main() {
+//char nam[] = "Shaheena Kovvuri Princess";
+char* name = strcpy(name , "Shaheena Kovvuri Princess");
+printf("Original = %s \n", name);
+int initial = 0, final;
+int length = strlen(name);
+ for( int i = 0 ; i <= length ; i++)
+ { 
+     if(*(name+i) == ' ' || *(name+i) == '\0')
+     { 
+         final = i-1;
+         str_rev(name, initial, final);
+         initial = i+1;
+     }
+ }
+ 
+  printf("Reverse = %s \n ", name);
+    return 0;
+    
+}
+*/
